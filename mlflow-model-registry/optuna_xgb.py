cb87d65e17fb6368bcaae94635c533f02da52059
ilya <ilya-illarionov@yandex.ru>
2022-08-19 09:24:27 +0300

    first commit

--- mlflow-model-registry/optuna_xgb.py
+++ mlflow-model-registry/optuna_xgb.py
@@ -12,16 +12,19 @@ from sklearn.metrics import mean_squared_error
 import xgboost as xgb
 
 import mlflow
+from mlflow.entities import ViewType
+from mlflow.tracking import MlflowClient
+
 
 from prefect import flow, task
 from prefect.task_runners import SequentialTaskRunner
 
 RS = 2022
 
-EXPERIMENT_NAME = "xgboost-v1"
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
-mlflow.set_experiment(EXPERIMENT_NAME)
-
+OPTUNA_EXPERIMENT_NAME = "xgboost-optuna"
+mlflow.set_tracking_uri("sqlite:///mlflow-locally.db")
+mlflow.set_experiment(OPTUNA_EXPERIMENT_NAME)
+mlflow.xgboost.autolog()
 
 @task
 def process_data(filename):
@@ -58,26 +61,24 @@ def split_vect_data_(data_train, data_val):
     categorical = ['PU_DO']
     numerical = ['trip_distance']
 
-    dv = DictVectorizer()
+    dictvec = DictVectorizer()
 
     train_dicts = data_train[categorical + numerical].to_dict(orient='records')
-    X_train = dv.fit_transform(train_dicts)
+    X_train = dictvec.fit_transform(train_dicts)
 
     val_dicts = data_val[categorical + numerical].to_dict(orient='records')
-    X_val = dv.transform(val_dicts)
+    X_val = dictvec.transform(val_dicts)
 
     target = 'duration'
     y_train = data_train[target].values
     y_val = data_val[target].values
 
-    return X_train, X_val, y_train, y_val, dv
-
+    return X_train, X_val, y_train, y_val, dictvec
 
 @task
 def dump_pickle(obj, filename):
     with open(filename, "wb") as f_out:
         return pickle.dump(obj, f_out)
-
 
 @task
 def objective(trial, train, valid, y_val):
@@ -94,8 +95,8 @@ def objective(trial, train, valid, y_val):
     }
 
     with mlflow.start_run():
-        mlflow.set_tag("model", "xgboost")
-        mlflow.log_params(params)
+        # mlflow.set_tag("model", "xgboost")
+        # mlflow.log_params(params)
         booster = xgb.train(
             params=params,
             dtrain=train,
@@ -104,49 +105,25 @@ def objective(trial, train, valid, y_val):
             early_stopping_rounds=15
         )
         y_pred = booster.predict(valid)
-        val_rmse = mean_squared_error(y_val, y_pred, squared=False)
-        print(f'rmse: {val_rmse}')
-        mlflow.log_metric("val_rmse", val_rmse)
-
-    return val_rmse
-
-
-@task
-def train_best_model(train, valid, y_val):
-    with mlflow.start_run():
-
-        best_params = {
-            'learning_rate': 0.3038814615100758,
-            'max_depth': 43,
-            'min_child_weight': 4,
-            'alpha': 2.1593266075444184,
-            'lambda': 6.7325152009283835,
-            'objective': 'reg:squarederror',
-            'seed': 2022
-        }
-        mlflow.set_tag("model", "xgboost_best_params_v1")
-        mlflow.log_params(best_params)
-
-        booster = xgb.train(
-            params=best_params,
-            dtrain=train,
-            num_boost_round=100,
-            evals=[(valid, 'validation')],
-            early_stopping_rounds=10
-        )
-
-        y_pred = booster.predict(valid)
         rmse = mean_squared_error(y_val, y_pred, squared=False)
-        mlflow.log_metric("rmse", rmse)
+        # mlflow.log_metric("rmse", rmse)
 
-        # Logged artifacts
-        mlflow.xgboost.log_model(booster, artifact_path="models_mlflow_xgb")
+    return {'loss': rmse}
 
 
 @flow(task_runner=SequentialTaskRunner())
-def run(train_path: str="./data/green_tripdata_2021-01.parquet",
+def main(train_path: str="./data/green_tripdata_2021-01.parquet",
          val_path:   str="./data/green_tripdata_2021-02.parquet",
-         dest_path:  str="./preprocess_data"):
+         dest_path: str="./preprocess_data"):
+
+    #Start tracking exp
+
+
+    # EXPERIMENT_NAME = "xgboost-best-models"
+    #
+    # mlflow.set_tracking_uri("http://127.0.0.1:5000")
+    # mlflow.set_experiment(EXPERIMENT_NAME)
+
 
     # Read and preprocess data
     X_train = process_data(train_path)
@@ -156,7 +133,7 @@ def run(train_path: str="./data/green_tripdata_2021-01.parquet",
     X_train, X_val = add_features(X_train, X_val)
 
     #Vectorize and Split data
-    X_train, X_val, y_train, y_val, dv = split_vect_data_(X_train, X_val)
+    X_train, X_val, y_train, y_val, dictvec = split_vect_data_(X_train, X_val)
 
     # create dest_path folder
     os.makedirs(dest_path, exist_ok=True)
@@ -165,15 +142,15 @@ def run(train_path: str="./data/green_tripdata_2021-01.parquet",
     train = xgb.DMatrix(X_train, label=y_train)
     valid = xgb.DMatrix(X_val, label=y_val)
 
-    # save data
+    # save datasets
     dump_pickle((X_train, y_train), os.path.join(dest_path, "train.pkl"))
     dump_pickle((X_val, y_val), os.path.join(dest_path, "test.pkl"))
-    dump_pickle((dv), os.path.join(dest_path, "dv.pkl"))
+    dump_pickle((dictvec), os.path.join(dest_path, "dv.pkl"))
 
     # #Search best params
     sampler = TPESampler(seed=RS)
     study = optuna.create_study(direction='minimize',
-                                sampler=sampler)
+                                    sampler=sampler)
     study.optimize(
         lambda trial: objective(
                             trial,
@@ -183,11 +160,9 @@ def run(train_path: str="./data/green_tripdata_2021-01.parquet",
         n_trials=5
                    )
 
-    train_best_model(train, valid, y_val)
 
 if __name__ == '__main__':
-    run()
-
+    main()
 
 # prefect deployment build ./prefect_flow1.py:main --name "Example Deployment"
 # prefect deployment apply ./main-deployment.yaml
